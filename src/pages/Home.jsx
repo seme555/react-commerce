129a46a3c190530cb8c0e5522f3be322751235d7
seme555 <semeabe@gmail.com>
2025-03-27 10:27:36 +0300

    !

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
         <div className="container mx-auto py-4 flex flex-col md:flex-row  space-x-2">
           <div className="w-full md:w-3/12">
             <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">
-              SHOP BY CATEGORIES
+              SHOP BY CATEGORIES!
             </div>
             <ul className="space-y-4 bg-grey-100 p-3 border">
               {Categories.map((cateory, index) => (
